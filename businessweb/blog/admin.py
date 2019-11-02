d6d4c107eeeefe1ea228787e9d995f2e4c44efa9
David Pablos <david.pablos@gmail.com>
2019-11-02 18:14:56 +0100

    Refactor blog urls and stuff. Customize admin

--- businessweb/blog/admin.py
+++ businessweb/blog/admin.py
@@ -7,6 +7,16 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at')
+    list_display = ('title', 'author', 'published_at', 'post_categories')
+    ordering = ('author', 'published_at')
+    search_fields = ('title', 'author__username', 'categories__name')
+    date_hierarchy = 'published_at'
+    list_filter = ('author__username', 'categories__name')
+
+    def post_categories(self, obj):
+        return ", ".join(c.name for c in obj.categories.all().order_by("name"))
+    
+    post_categories.short_description = "Categorías"
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
