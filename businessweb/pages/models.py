96154a8c834f753f88265f0eb602a746b41aaf69
David Pablos <david.pablos@gmail.com>
2019-11-02 21:30:32 +0100

    Install and custom ckeditor

--- businessweb/pages/models.py
+++ businessweb/pages/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 class Page(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
-    content = models.TextField(verbose_name="Contenido")
+    content = RichTextField(verbose_name="Contenido")
     order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
-
 
     class Meta:
         verbose_name = "página"
